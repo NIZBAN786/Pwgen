890d0d2f32eb71f6170597ce710b09cfd8b411a6
Nizban Akbar Khoja <nizbankhoja25@gmail.com>
2025-07-12 17:29:16 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
 
 def run_flask():
     """Run the Flask application."""
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=1100, debug=False)
 
 if __name__ == '__main__':
     run_flask() 
